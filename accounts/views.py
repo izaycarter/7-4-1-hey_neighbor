06b937235968b5da6db787c58a0278b923979a4b
Isaiah Carter <izay.carter0509@gmail.com>
2019-10-10 17:08:53 -0400

    url for login and sign up

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.views import generic
+from django.urls import reverse_lazy
 
 # Create your views here.
 
--- tools/models.py
+++ tools/models.py
@@ -1,7 +1,10 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-
+User = get_user_model()
 # Create your models here.
 
-class Neighbor(models.Model):
+class Tool(models.Model):
     name = models.CharField(max_length=255)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    is_available = models.BooleanField(default="True")
