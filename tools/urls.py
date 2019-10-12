10202c725c16d219bdfdfb6f8b55dfb694720cbd
Isaiah Carter <izay.carter0509@gmail.com>
2019-10-12 10:06:39 -0400

    home links connected

--- tools/urls.py
+++ tools/urls.py
@@ -7,5 +7,8 @@ app_name = "tools"
 urlpatterns = [
 path("", views.StartPage.as_view(), name="startpage"),
 path("my_tools/", views.MyToolsView.as_view(), name="mytools"),
-path("new_tool/", views.NewToolView.as_view(), name="newtool")
+path("new_tool/", views.NewToolView.as_view(), name="newtool"),
+path("delete/<str:pk>", views.DeleteToolView.as_view(), name="deletetool"),
+path("neighbors/",views.NeighborsView.as_view(), name="neighbors"),
+path("borrower_feed/", views.BorrowerView.as_view(), name="borrower_feed")
 ]
--- tools/views.py
+++ tools/views.py
@@ -6,6 +6,9 @@ from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.auth.decorators import user_passes_test
 from django.urls import reverse, reverse_lazy
 from django.views import generic
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
 class StartPage(generic.TemplateView):
@@ -15,10 +18,45 @@ class StartPage(generic.TemplateView):
 class MyToolsView(LoginRequiredMixin, generic.ListView):
     template_name = "tools/my_tools.html"
     model = Tool
+    context_object_name = "tool_list"
+    def get_queryset(self):
+        return Tool.objects.filter(owner=self.request.user)
 
 
 class NewToolView(LoginRequiredMixin, generic.CreateView):
     model = Tool
-    # fields = '__all__'
-    fields = ["name","is_available"]
+    fields = ["name","type","is_available",]
     template_name = "tools/new_tool.html"
+
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
+
+    success_url = reverse_lazy("tools:mytools")
+
+
+class DeleteToolView(LoginRequiredMixin, generic.DeleteView):
+    model = Tool
+
+    def get_object(self, queryset=None):
+        tool = super(DeleteToolView, self).get_object()
+        if not tool.owner == self.request.user:
+            raise Http404
+        return tool
+    success_url = reverse_lazy("tools:mytools")
+
+
+class NeighborsView(generic.ListView):
+    model = User
+    template_name = "tools/neighbors.html"
+    context_object_name = "neighbors_list"
+    def get_queryset(self):
+        return User.objects.exclude(username="admin")
+
+class BorrowerView(LoginRequiredMixin,generic.ListView):
+    model = Tool
+    template_name = "tools/borrower.html"
+    context_object_name = "borrower_list"
+
+    def get_queryset(self):
+        return Tool.objects.all()
